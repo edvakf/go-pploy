aa87853bba4b6be2b76e254e8483b36bd9eb8072
Atsushi Takayama <taka.atsushi@gmail.com>
2018-02-19 10:48:28 +0900

    Remove old command

--- unbuffered/command.go
+++ unbuffered/command.go
@@ -3,9 +3,6 @@ package unbuffered
 import "os/exec"
 
 // libc does not line buffer if output is not a terminal, instead use full buffering.
-// `script` runs a given command in a pseudo terminal.
-// it also redirects stderr to stdout which fits our usage
-
 // see: http://unix.stackexchange.com/questions/25372/turn-off-buffering-in-pipe
 
 var stdbuf string
